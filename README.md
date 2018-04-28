a47df1bd21e0283b1e1b38428d123c60db131404
goodwjf <wangjianfenga1@163.com>
2018-04-28 16:54:40 +0800

    更新readme 选中元素改为数据绑定的形式 修正选中元素上下移动的时候超出可视区滚动条不跟随滚动的问题

--- README.md
+++ README.md
@@ -4,3 +4,27 @@
 * 支持tag的添加
 * 支持tag的删除
 * 支持tag的搜索
+
+### 交换数组元素参考
+ ```javascript
+    var swapItems = function(arr, index1, index2) {
+        arr[index1] = arr.splice(index2, 1, arr[index1])[0];
+        return arr;
+    };
+
+    // 上移
+    $scope.upRecord = function(arr, $index) {
+        if($index == 0) {
+            return;
+        }
+        swapItems(arr, $index, $index - 1);
+    };
+
+    // 下移
+    $scope.downRecord = function(arr, $index) {
+        if($index == arr.length -1) {
+            return;
+        }
+        swapItems(arr, $index, $index + 1);
+    };
+```
--- src/components/TagList/index.js
+++ src/components/TagList/index.js
@@ -23,12 +23,29 @@
   sortData = { mobx.action } 
 */
 
+function onOverflowContainer(container, element, direction) {
+  const offsetBox = container.getBoundingClientRect().top
+  const box = element.getBoundingClientRect()
+  const containerHeight = container.clientHeight
+  const elementHeight = element.offsetHeight  
+  let scrollUp = box.top - offsetBox - elementHeight // (box.top < offsetBox)
+  let scrollDown = box.bottom - containerHeight - offsetBox + elementHeight// (box.bottom > containerHeight + offsetBox)
+
+  if (direction === 'up' && scrollUp < 0) {
+    container.scrollTop += scrollUp
+  }
+
+  if (direction === 'down' && scrollDown > 0) {
+    container.scrollTop += scrollDown
+  }
+}
+
 import { observer } from 'mobx-react'
 import React, { Component } from 'react'
-import { Tag, Input, Tooltip, Icon, Modal, Pagination, Button } from 'antd';
+import { Tag, Input, Tooltip, Icon, Modal, Pagination, Button } from 'antd'
 import css from './index.scss'
-const confirm = Modal.confirm;
-const Search = Input.Search;
+const confirm = Modal.confirm
+const Search = Input.Search
 
 @observer
 class MyTag extends Component {
@@ -42,7 +59,7 @@ class MyTag extends Component {
         node: (<Icon type="plus" className={css['icon-action']} />)
       },
       'close': {
-        className: [css['my-tag']].join(' '),
+        className: [css['my-tag'], props.className].join(' '),
         node: (<Icon type="close" className={css['icon-action']} onClick={props.icon.event} />)
       }
     }
@@ -67,6 +84,7 @@ export default class TagList extends Component {
   state = {
     inputVisible: false,
     inputValue: '',
+    selectedValue:''
   };
 
   componentDidMount() {
@@ -74,15 +92,13 @@ export default class TagList extends Component {
   }
 
   handleClose = (e) => {
-    let removedTag = e.target.parentNode.textContent
+    let removedTag = this.state.selectedValue
     confirm({
       title: '提示！',
       content: '确认要删除吗？',
       cancelText: '否',
       okText: '是',
       onOk: () => {
-        // const data = this.state.data.filter(tag => tag !== removedTag);
-        this.selectedTagNode && this.selectedTagNode.classList.add(css['tag-selected'])
         this.props.removeData(removedTag)
         this.props.onRemoved(removedTag, this.props.data)
       }
@@ -91,12 +107,9 @@ export default class TagList extends Component {
   }
 
   handleSelected = (e) => {
-    if (this.selectedTagNode) {
-      this.selectedTagNode.classList.remove(css['tag-selected'])
-    }
-    this.selectedTagNode = e.target
-    this.selectedTagNode.classList.add(css['tag-selected'])
-    this.props.onSelected(this.selectedTagNode.textContent)
+    this.setState({selectedValue: e.target.dataset.val}, () => {
+      this.props.onSelected(this.state.selectedValue)
+    })
   }
 
   showInput = () => {
@@ -127,14 +140,18 @@ export default class TagList extends Component {
   }
 
   handleArrow = (direction) => {
-    this.props.sortData(this.selectedTagNode.textContent, direction)
+    this.state.selectedValue && this.props.sortData(this.state.selectedValue, direction)
+    // 修正：当焦点元素超出容器滚动条不自动滚动的问题（Chrome v64 只支持向上滚动）
+    let container = document.querySelector(`.${css.box}`)
+    let element = document.querySelector(`.${css['tag-selected']}`)
+    element && onOverflowContainer(container, element, direction)
   }
 
   searchInputRef = input => this.searchInput = input
   saveInputRef = input => this.input = input
 
   render() {
-    const { inputVisible, inputValue } = this.state
+    const { inputVisible, inputValue, selectedValue } = this.state
     const { width, height, plusName, searchVisible, data, plus, arrow } = this.props
     return (
       <div>
@@ -148,30 +165,29 @@ export default class TagList extends Component {
               style={{ width: 200 }}
               size="small"
             />
-         
             {arrow && (<Button icon="arrow-up" size="small" className={css.arrow} onClick={() => this.handleArrow('up')}/>)}
             {arrow && (<Button icon="arrow-down" size="small" className={css.arrow} onClick={() => this.handleArrow('down')}/>)}
-    
         </div>
         )}
         <div className={css.box} style={{width, height}}>
-          {data.map((tag, index) => {
-            const isLongTag = tag.length > 20;
+          {data.map((val, index) => {
+            const isLongVal = val.length > 20;
             const tagElem = (
               <MyTag 
-                key={tag}
+                key={val}
                 icon={{
                   type: 'close',
                   event: this.handleClose
                 }}
+                className={selectedValue === val ? css['tag-selected'] : ""}
                 index={index}
-                val={tag}
+                val={val}
                 onClick = {this.handleSelected}
               >
-                {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                {isLongVal ? `${val.slice(0, 20)}...` : val}
               </MyTag>
             );
-            return isLongTag ? <Tooltip title={tag} key={tag}>{tagElem}</Tooltip> : tagElem;
+            return isLongVal ? <Tooltip title={val} key={val}>{tagElem}</Tooltip> : tagElem;
           })}
           {plus && inputVisible && (
             <Input
